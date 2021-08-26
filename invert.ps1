82f56301949658fd9eef58d96e3a4830e8123884
Kevin Oesef <kevinoesef@gmail.com>
2021-08-26 12:14:27 -0700

    Initial commit, auto inverter

---
+++ invert.ps1
@@ -0,0 +1,15 @@
+﻿#worx
+$outputExtension = ".tif"
+$inputBaseName = dir -File | select -expand BaseName
+$currentPath = Get-Location | select -expand Path
+
+$toExecute = 'magick.exe'
+$toExecute_Mode = 'convert'
+$toExecute_Mode2 = '-channel RGB -negate'
+
+$dirname = "inverted"
+mkdir $dirname
+$outputPath = $currentPath+'\'+$dirname+'\'
+
+#convert input.png -channel RGB -negate output.png
+$inputBaseName | ForEach-Object {&magick.exe convert $_$outputExtension -channel RGB -negate $outputPath$_$outputExtension}
